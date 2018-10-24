4f76167830cc50708bda3f12439ac434a23c34e2
seandolinar <sjjd199@gmail.com>
2018-10-24 08:31:12 -0400

    added google site verification

--- src/components/PostThumbnail.js
+++ src/components/PostThumbnail.js
@@ -4,14 +4,24 @@ import { Link } from 'gatsby'
 class PostThumbnail extends React.Component {
   constructor(props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this);
   }
  
+  // handleClick() {
+  //   fetch('https://stats.seandolinar.com', {method: 'post', body: JSON.stringify({test: 1})}).then(res => console.log(res))
+  // }
+  
+
   render() {
       return <Link to={'/' + this.props.slug}><div className="post-thumbnail grid-item">
       <img src={this.props.imgUrl} />
       <h4 className="post-thumbnail-title" dangerouslySetInnerHTML={{ __html: this.props.title }} /> 
-    </div></Link>
+    </div>    
+</Link>
   }
 }
 
 export default PostThumbnail
+
+
+//<button onClick={this.handleClick}>test me</button>
