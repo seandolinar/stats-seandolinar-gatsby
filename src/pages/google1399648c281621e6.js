437bd2fbe009ea583fb84b495f02973fe2e09857
seandolinar <sjjd199@gmail.com>
2018-10-24 09:35:22 -0400

    in a component

--- src/pages/google1399648c281621e6.js
+++ src/pages/google1399648c281621e6.js
@@ -1,1 +1,10 @@
-`google-site-verification: google1399648c281621e6.html`
+import React from 'react'
+
+export default () => (
+    <>
+      `google-site-verification: google1399648c281621e6.html`
+    </>
+  )
+
+
+
